e3fc09ca7479b88e5e38a448b932dbff1babc52a
Xavier Tolza <tolza.xavier@gmail.com>
2018-02-14 10:43:03 +0100

    First commit

---
+++ compile_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install -y build-essential git qt5-default qt5-qmake libpq5 libxml2 pkg-config xml2 libxml2-dev libpq-dev &&
+sudo apt-get install -y libqt5svg5* &&
+git clone https://github.com/pgmodeler/pgmodeler.git &&
+cd pgmodeler &&
+qmake pgmodeler.pro &&
+make &&
+sudo make install
